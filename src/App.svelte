844010233e4e4d367fd1f2fac7e76dad87424f79
Valentina Vitetta <45749910@utdt.local>
2024-03-19 17:40:52 -0300

    Clase 2: creación de app base + estilos css

--- src/App.svelte
+++ src/App.svelte
@@ -3,7 +3,21 @@
 </script>
 
 <!-- Estructura contenido HTML -->
+<h1>Meta de Lectura Anual</h1>
+<br>
+<div>
+    <h5 id="mes">Mayo 2023</h5>
+    <h5 id="mes">Diciembre 2023</h5>
+</div>
 
+<div>
+    <img src="public\images\libros.svg" alt="librosMayo" width=20%>
+    <img src="public\images\libros1.svg" alt="librosDiciembre" width=20%>
+</div>
+<div>
+    <h5 id="porcentaje"> 25%</h5>
+    <h5 id="porcentaje">100%</h5>
+</div>
 <!-- Estilos CSS -->
 <style>
 </style>
