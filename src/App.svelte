79f522871c9bc28856992a9d64891d5aa70908b1
valenvv <valentinavitetta5@gmail.com>
2024-03-24 19:37:12 -0300

    version 2 tp: con modificaciones de la clase

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,36 @@
 <!-- Script JS -->
 <script>
+    import * as d3 from "d3"
+    import BooksColumns from "./lib/BooksColumns.svelte"
+  
+    let numbers = [5, 18, 29, 75, 100]
+    let numbersDivided5 = numbers.map(n => Math.floor(n / 5))
+  
+    function altura(n) {
+      let scale = d3
+        .scaleLinear()
+        .domain(d3.extent(numbers)) // extent returns [min, max]
+        .range([15, 225])
+      return scale(n)
+    }
 </script>
 
-<!-- Estructura contenido HTML -->
+<!-- Estilos-->
+<main>
+    <h3 class="headline">Meta de Lectura Anual</h3>
+    <BooksColumns numbers={numbers} />
+    
+</main>
+
+<style>
+    .headline {
+      text-align: left;
+      margin-top: 50px;
+      /* border: 1px solid red; */
+    }
+  </style>
+
+<!-- Estructura contenido HTML CLASE 1 
 <h1>Meta de Lectura Anual</h1>
 <br>
 <div>
@@ -18,6 +46,5 @@
     <h5 id="porcentaje"> 25%</h5>
     <h5 id="porcentaje">100%</h5>
 </div>
-<!-- Estilos CSS -->
-<style>
-</style>
+ -->
+
---
+++ src/lib/BooksColumns.svelte
@@ -0,0 +1,61 @@
+<script>
+    import * as d3 from "d3"
+  
+    export let numbers = []
+  
+    function alturaColBooks(n) {
+      let scale = d3
+        .scaleLinear()
+        .domain([0, d3.max(numbers)]) // extent returns [min, max]
+        .range([0, 100])
+      return scale(n)
+    }
+  
+  </script>
+  
+  
+  <div class="books-container">
+    {#each numbers as n}
+    <div>
+      <div class="column-wrapper">
+        <div class="column-books" style="height: {alturaColBooks(n)}%;"></div>
+        <img class="books" src="./images/books.png" alt="">
+      </div>
+      <p class="number">{n}</p>
+    </div>
+    {/each}
+  </div>
+  
+  <style>
+    .books-container {
+      display: flex;
+    }
+    .column-wrapper {
+      position: relative;
+      width: 160px;
+      height: 190px;
+      background-color: #d4d4d4;
+      margin-bottom: 10%;
+      margin-top: 10%;
+      /*border: black 1px solid; */
+    }
+    .column-books {
+      position: absolute;
+      height: 50%;
+      left: 10%;
+      right: 10%;
+      bottom: 0;
+      /* width: 100%; */
+      background-color: #2BD127;
+    }
+    .books {
+      position: absolute;
+      bottom: -10px;
+      width: 100%;
+    }
+    .number {
+      text-align: center;
+
+    }
+  </style>
+  
